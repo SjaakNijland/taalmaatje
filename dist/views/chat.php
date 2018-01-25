ffdf39295267cc469ae4701198af4c35ebca5821
Robbsnor <robbinschmidt@gmail.com>
2018-01-25 02:32:50 +0100

    profile
    - fixed width on profile, now its completeley visible
    - better HTML indenting

--- dist/views/chat.php
+++ dist/views/chat.php
@@ -1,126 +1,122 @@
 <div id="profile-container">
 
-		<div class="profile">
+	<div class="profile">
+		<div class="avatar" style="background-image: url(https://ih0.redbubble.net/image.363915427.3586/flat,800x800,075,t.u2.jpg)">
 
-			<div class="avatar" style="background-image: url(https://ih0.redbubble.net/image.363915427.3586/flat,800x800,075,t.u2.jpg)">
+			<div class="icons">
 
-				<div class="icons">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12">
 
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col-sm-12">
+							<ul class="no-dots">
+								<li><a href="" class="like ion-android-star-outline"></a></li>
+								<li><a href="" class="heart active ion-heart"></a></li>
+							</ul>
 
-								<ul class="no-dots">
-									<li><a href="" class="like ion-android-star-outline"></a></li>
-									<li><a href="" class="heart active ion-heart"></a></li>
-								</ul>
-
-							</div>
 						</div>
 					</div>
-
-
 				</div>
-
+				
 			</div>
 
 		</div>
+	</div>
 
 
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm-12">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
 
-					<div class="info">
-
-						<h2>Trissa Winslow (26)</h2>
-
-						<h4><b>About me</b></h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ab saepe sequi vel suscipit, corporis ipsum, laudantium animi distinctio provident dolore quod repellendus, ratione autem, aperiam rem eos dolorum vero.</p>
-
-					</div>
-
+				<div class="info">
+					<h2>Trissa Winslow (26)</h2>
+					<h4><b>About me</b></h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ab saepe sequi vel suscipit, corporis ipsum, laudantium animi distinctio provident dolore quod repellendus, ratione autem, aperiam rem eos dolorum vero.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ab saepe sequi vel suscipit, corporis ipsum, laudantium animi distinctio provident dolore quod repellendus, ratione autem, aperiam rem eos dolorum vero.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ab saepe sequi vel suscipit, corporis ipsum, laudantium animi distinctio provident dolore quod repellendus, ratione autem, aperiam rem eos dolorum vero.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ab saepe sequi vel suscipit, corporis ipsum, laudantium animi distinctio provident dolore quod repellendus, ratione autem, aperiam rem eos dolorum vero.</p>
 				</div>
+
 			</div>
 		</div>
-
 	</div>
+</div>
 
-	<div id="profile-backdrop" class="profile-close"></div>
-
-
-
-
-
-	<div id="video-chat-container">
-
-        <div id="video-chat-them"></div>
-        <div id="video-chat-you"></div>
-
-		<div class="controls">
-
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12">
-
-            			<ul class="no-dots">
-            				<li><a href="" class="ion-ios-telephone"></a></li>
-            				<li><a href="" class="ion-ios-videocam"></a></li>
-            				<li><a href="" class="ion-android-microphone"></a></li>
-            				<li><a href="" class="chat-toggle ion-chatbubbles"></a></li>
-            			</ul>
-
-                    </div>
-                </div>
-            </div>
-
-		</div>
-
-	</div>
+<div id="profile-backdrop" class="profile-close"></div>
 
 
 
-    <div id="text-chat-container">
+
+
+<div id="video-chat-container">
+
+    <div id="video-chat-them"></div>
+    <div id="video-chat-you"></div>
+
+	<div class="controls">
 
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-12">
 
-                    <div id="text-chat">
-
-                        <div class="bubble bubble-them">Hello i am Trissa!</div>
-                        <div class="bubble bubble-you">Hello im John, how are you doing today?</div>
-                        <div class="bubble bubble-them">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</div>
-                        <div class="bubble bubble-you">Suspendisse fringilla eros ut felis scelerisque, id accumsan felis sollicitudin. Integer tempor purus id est suscipit, et consequat libero condimentum.</div>
-                        <div class="bubble bubble-you">Vivamus ultricies pretium dui. Cras porta sollicitudin ornare. Aenean imperdiet urna risus. Quisque in sapien in velit ullamcorper interdum.</div>
-                        <div class="bubble bubble-them">Pellentesque nec turpis ac massa tempor mattis ut ac diam. Proin egestas lacinia lacus, nec vulputate tellus bibendum eu. Suspendisse nec egestas est.</div>
-                        <div class="bubble bubble-you">Curabitur in lectus vitae metus commodo egestas non a erat.</div>
-                        <div class="bubble bubble-you">Cras arcu ligula, dignissim nec interdum ac, consectetur at nisi.</div>
-                        <div class="bubble bubble-them">Vivamus augue velit, tempus sed luctus tempor, finibus eleifend urna. Ut convallis efficitur auctor.</div>
-                        <div class="bubble bubble-them">Vestibulum aliquet suscipit leo.</div>
-                        <div class="bubble bubble-you">Si.</div>
-
-                    </div>
+        			<ul class="no-dots">
+        				<li><a href="" class="ion-ios-telephone"></a></li>
+        				<li><a href="" class="ion-ios-videocam"></a></li>
+        				<li><a href="" class="ion-android-microphone"></a></li>
+        				<li><a href="" class="chat-toggle ion-chatbubbles"></a></li>
+        			</ul>
 
                 </div>
             </div>
         </div>
 
-        <div id="text-send-container">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12">
+	</div>
 
-                            <form id="form-send-text" action="">
+</div>
 
-                                <input type="text" id="type-area" class="form-control" placeholder="Type a message">
-                                <button type="submit" id="text-send-button" class="btn btn-primary"><i class="ion-android-send"></i></button>
 
-                            </form>
 
-                    </div>
+<div id="text-chat-container">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+
+                <div id="text-chat">
+
+                    <div class="bubble bubble-them">Hello i am Trissa!</div>
+                    <div class="bubble bubble-you">Hello im John, how are you doing today?</div>
+                    <div class="bubble bubble-them">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</div>
+                    <div class="bubble bubble-you">Suspendisse fringilla eros ut felis scelerisque, id accumsan felis sollicitudin. Integer tempor purus id est suscipit, et consequat libero condimentum.</div>
+                    <div class="bubble bubble-you">Vivamus ultricies pretium dui. Cras porta sollicitudin ornare. Aenean imperdiet urna risus. Quisque in sapien in velit ullamcorper interdum.</div>
+                    <div class="bubble bubble-them">Pellentesque nec turpis ac massa tempor mattis ut ac diam. Proin egestas lacinia lacus, nec vulputate tellus bibendum eu. Suspendisse nec egestas est.</div>
+                    <div class="bubble bubble-you">Curabitur in lectus vitae metus commodo egestas non a erat.</div>
+                    <div class="bubble bubble-you">Cras arcu ligula, dignissim nec interdum ac, consectetur at nisi.</div>
+                    <div class="bubble bubble-them">Vivamus augue velit, tempus sed luctus tempor, finibus eleifend urna. Ut convallis efficitur auctor.</div>
+                    <div class="bubble bubble-them">Vestibulum aliquet suscipit leo.</div>
+                    <div class="bubble bubble-you">Si.</div>
+
                 </div>
+
             </div>
         </div>
-
     </div>
+
+    <div id="text-send-container">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+
+                        <form id="form-send-text" action="">
+
+                            <input type="text" id="type-area" class="form-control" placeholder="Type a message">
+                            <button type="submit" id="text-send-button" class="btn btn-primary"><i class="ion-android-send"></i></button>
+
+                        </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
